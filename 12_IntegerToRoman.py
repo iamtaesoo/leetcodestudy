57617307ee45647f4cbdb6a7943fc805fb294d6d
iamtaisoo <iamtaesoo@gmail.com>
2024-01-21 21:21:02 -0800

    [medium][11] Integer to Roman

---
+++ 12_IntegerToRoman.py
@@ -0,0 +1,63 @@
+'''
+    https://leetcode.com/problems/integer-to-roman/description/
+    12. Integer to Roman
+    Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+    Symbol       Value
+    I             1
+    V             5
+    X             10
+    L             50
+    C             100
+    D             500
+    M             1000
+    For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+    Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it making four. The same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+    I can be placed before V (5) and X (10) to make 4 and 9. 
+    X can be placed before L (50) and C (100) to make 40 and 90. 
+    C can be placed before D (500) and M (1000) to make 400 and 900.
+    Given an integer, convert it to a roman numeral.
+'''
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        out = ''        
+        I = num % 10        
+        if I == 4:
+            out = 'IV' + out 
+        elif I >= 5 and I < 9:
+            out = 'V' + 'I' * (I - 5) + out
+        elif I == 9:
+            out = 'IX' + out
+        else:        
+            out = out + 'I' * I      
+
+        num //= 10
+        X = num % 10
+        if X == 4:
+            out = 'XL' + out
+        elif X >= 5 and X < 9:
+            out = 'L' + 'X' * (X - 5) + out
+        elif X == 9:
+            out = 'XC' + out
+        else:        
+            out = 'X' * X  + out
+        
+        num //= 10
+        C = num % 10       
+
+        if C == 4:
+            out = 'CD' + out
+        elif C >= 5 and C < 9:
+            out = 'D' + 'C' * (C - 5) + out
+        elif C == 9:
+            out = 'CM' + out
+        else:        
+            out = 'C' * C + out
+
+        num //= 10
+        M = num % 10         
+        out = 'M' * M + out
+        return out
